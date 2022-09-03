a6546accbc6293e736bdce83e4e4a60a52cd9574
bcb4303 <bcb4303@rit.edu>
2022-09-03 15:06:09 -0400

    overlay

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import { MdClose } from "react-icons/md";
+import axios from 'axios';
 import './Maps.css';
 const api_key = 'AIzaSyDH-KRabVOXL9wpJvRoeMJRNvGLn9Qd9wI';
 
@@ -16,20 +17,30 @@ const center = {
 
 
 function Map() {
+    const [pins, setPins] = useState([]);
     const { isLoaded } = useJsApiLoader({
         id: 'google-map-script',
         googleMapsApiKey: api_key
     })
 
     const [map, setMap] = React.useState(null)
+
+
     const getMarkers= () => {
-        /*
-        */
+        axios.get(
+            '/api/getpisns'
+        ).then(res => {
+            console.log(res.data);
+            setPins(res.data)
+        });
     }
+
+
 
     const onLoad = React.useCallback(function callback(map) {
         const bounds = new window.google.maps.LatLngBounds(center);
         map.fitBounds(bounds);
+        getMarkers();
         setMap(map)
     }, [])
 
@@ -37,7 +48,6 @@ function Map() {
         setMap(null)
     }, [])
     const onClick = React.useCallback(function callback(event, item) {
-        console.log(event.latLng.lat(), event.latLng.lng())
         if (item === 'marker') {
             var popup = document.getElementById("overlay");
             console.log(popup);
@@ -64,25 +74,38 @@ function Map() {
         >
             <Marker
                 key={'test2'}
-                title={"This is a title"}
                 position={{ lat: 43.084737, lng: -77.6791509 }}
                 onClick={e => onClick(e, 'marker')}
-            >
-                <div id="overlay">
-                    <div className='close-overlay' onClick={e => onClick(e, 'map')}>
-                        <MdClose style={{'width': '64px', 'height':'64px'}} />
+                >
+                <div id="overlay" onClick={e => onClick(e, 'marker')}>
+                    <div className='close-overlay'>
+                            <MdClose class='close-button' />
+                        </div>
+                    <div className='popup'>
+                        <h2>Title</h2>
+                        <div>Overlay Text</div>
                     </div>
-                    <div id="text">Overlay Text</div>
                 </div>
           </Marker>
+            {pins.map(pin => (
+                <Marker
+                    key={pin._id}
+                    position={{ lat: pin.latitude, lng: pin.longitude }}
+                    onClick={e => onClick(e, 'marker')}
+                >
+                    <div id="overlay" onClick={e => onClick(e, 'marker')}>
+                        <div className='close-overlay'>
+                            <MdClose class='close-button' />
+                        </div>
+                        <div className='popup'>
+                            <h2>{pin.title}</h2>
+                            <div>{pin.description}</div>
+                        </div>
+                    </div>
+                </Marker>
+            ))}
 
-            <Marker
-                key={'test2'}
-                title={"This is a title"}
-                position={{ lat: 43.0839635, lng: -77.6764336 }}
-                onClick={onClick}
-                
-            />
+            {/*id, title, text, lat, lng, catagory"*/}
         </GoogleMap>
     ) : <></>
 }
