dd6e37d32c82d91fbbad1184dacc83b4cc3a8816
Luke Zimmerman <lukezimm2014@gmail.com>
2022-09-03 13:52:56 -0400

    ef context

---
+++ RITMap/Context/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RITMap.Models;
+
+namespace RITMap.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public ApplicationContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(Configuration.GetConnectionString("WebApiDatabase"));
+        }
+
+        public DbSet<Pin> Pins { get; set; }
+
+    }
+}
---
+++ RITMap/Controllers/ApiController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RITMap.Models;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Security.Claims;
+using Microsoft.Extensions.Hosting;
+using System.Xml.Linq;
+using RITMap.Context;
+
+namespace RITMap.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+
+        private readonly ApplicationContext _dbcontext;
+
+        public ApiController(ApplicationContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpGet("getpins")]
+        public List<Pin> GetPins()
+        {
+            // get post object
+            return _dbcontext.Pins.ToList();
+        }
+
+        [HttpPost("createpin")]
+        public void CreatePin(Pin pin)
+        {
+            _dbcontext.Pins.Add(pin);
+            _dbcontext.SaveChanges();
+        }
+    }
+}
