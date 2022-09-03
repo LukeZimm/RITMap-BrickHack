8ad7be851b8ab152cc38d5da360387ec1019cfff
bcb4303 <bcb4303@rit.edu>
2022-09-03 14:36:01 -0400

    map overlay

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
 import { MdBlurOn } from "react-icons/md";
-
+import './Maps.css';
 const api_key = 'AIzaSyDH-KRabVOXL9wpJvRoeMJRNvGLn9Qd9wI';
 
 const containerStyle = {
     width: '1200px',
-    height: '800px'
+    height: 'calc(100vh - 100px)'
 };
 
 const center = {
@@ -37,30 +37,36 @@ function Map() {
         if (item === 'marker') {
             var popup = document.getElementById("overlay");
             console.log(popup);
+            
             if (popup.style.display !== "block") {
                 popup.style.display = "block";
             } else {
                 popup.style.display = "none";
             };
         }
+        else if (item ==='map') {
+
+            
+        }
     }, []);
 
     return isLoaded ? (
         <GoogleMap
-            mapContainerStyle={containerStyle}
             zoom={16}
             onLoad={onLoad}
             onUnmount={onUnmount}
-            onClick={onClick}
+            onClick={e => onClick(e, 'map')}
+            mapContainerClassName="map"
         >
             <Marker
                 key={'test2'}
                 title={"This is a title"}
                 position={{ lat: 43.084737, lng: -77.6791509 }}
                 onClick={e => onClick(e, 'marker')}
-            ><div id="overlay" onclick="off()">
-            <div id="text">Overlay Text</div>
-          </div>
+            >
+                <div id="overlay">
+                    <div id="text">Overlay Text</div>
+                </div>
           </Marker>
 
             <Marker
