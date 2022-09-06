acb4c0f7dfe71ec82cd3fbec129e26cdef0354d9
bcb4303 <bcb4303@rit.edu>
2022-09-06 15:27:02 -0400

    Ui Revamp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
-import GoogleMap from './components/Map';
-import Nav from './components/Navbar';
-//AIzaSyDH-KRabVOXL9wpJvRoeMJRNvGLn9Qd9wI
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-const handleApiLoaded = (map, maps) => {
-  // use map and maps objects
-};
-
+import Nav from './components/Nav';
+import Map from './components/Map';
 
 function App() {
   return (
-    <div>
+    <>
       <Nav/>
-      <GoogleMap/>
-    </div>
+      <Map/>
+    </>
   );
 }
 
